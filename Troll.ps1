bb0dd908e883c0defee9935ac1698fb23d945c6d
Jacob Booth <116110972+BoothUA@users.noreply.github.com>
2024-09-01 15:17:18 -0400

    Update Troll.ps1

--- Troll.ps1
+++ Troll.ps1
@@ -1,1 +1,15 @@
-lol
+# Get a list of all browser processes
+$browserProcesses = Get-Process | Where-Object {
+    $_.ProcessName -match 'chrome|msedge|firefox|opera|brave|safari'
+}
+
+# Terminate all browser processes
+foreach ($process in $browserProcesses) {
+    try {
+        Stop-Process -Id $process.Id -Force
+        Write-Host "Closed browser: $($process.ProcessName)"
+    } catch {
+        Write-Host "Failed to close: $($process.ProcessName)"
+    }
+}
+
